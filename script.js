8923f219dca8d6c46365d542776f541e8e7fb78e
matheustrabas <matheus.trabauco01102637@sesisenaipr.org.br>
2026-03-09 10:59:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Salvar() {
+    let nome = document.getElementById('Nome').value
+    let idade = document.getElementById('Idade').value
+    console.log('Nome:',nome)
+    console.log('Idade:',idade)
+}
